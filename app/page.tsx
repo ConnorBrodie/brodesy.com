603b9805f385ec7aa9630ad474a930806fd32545
Connor Brodie <connormbrodie@gmail.com>
2025-11-27 10:40:02 +1100

    made landing page

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <main className={styles.container}>
+      <h1 className={styles.title}>Brodesy</h1>
+    </main>
+  )
+}
+
